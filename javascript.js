00bb4e1d18b7894220889b496c8609a48e469d22
Shaban Hamiti <84515408+ShabanHamiti@users.noreply.github.com>
2022-01-07 16:51:56 +0100

    Amélioration mode sombre

--- javascript.js
+++ javascript.js
@@ -3,8 +3,10 @@ let h2 = document.querySelector("h2")
 let part1 = document.querySelector(".part__1")
 
 let modeSombre = function(){
-    part1.classList.toggle("invert")
-    h2.classList.toggle("invert")
+    part1.classList.toggle("invert__1")
+    h2.classList.toggle("invert__1")
+    part1.classList.toggle("invert__0")
+    h2.classList.toggle("invert__0")
 }
 
 header.addEventListener('click', modeSombre)
